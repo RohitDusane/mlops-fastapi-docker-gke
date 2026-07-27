c8fd87ed61a938ef66deda1ae00cfe0d6fa1451b
RohitDusane <stat.data247@gmail.com>
2026-07-27 21:33:41 +0530

    Fix package imports for CI tests

--- tests/test_data_ingestion.py
+++ tests/test_data_ingestion.py
@@ -1,4 +1,4 @@
 def test_data_ingestion_import():
-    from src.diabetes_risk_prediction.components import data_ingestion
+    from diabetes_risk_prediction.components import data_ingestion
 
     assert data_ingestion is not None
